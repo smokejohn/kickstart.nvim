ef1146b67e0119eef1aae5dabf8dc3db080308c1
Johannes Rauch <johannesrauch91@gmail.com>
2025-04-21 01:06:32 +0200

    Adds neogen plugin and keymap to generate docstrings depending on context

--- init.lua
+++ init.lua
@@ -142,7 +142,14 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-
+  {
+    'danymat/neogen',
+    config = function()
+      require('neogen').setup {
+        vim.keymap.set('n', '<Leader>d', '<cmd>Neogen<CR>', { desc = 'Generate [D]ocumentation' }),
+      }
+    end,
+  },
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
